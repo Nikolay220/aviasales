4de180875fc88761f9e1d83ddc5323f0a75a01a5
Nikolay Shutikov <nikolayshutikov@gmail.com>
2022-05-09 04:34:30 +0700

    учёт пересадок туда и обратно

--- src/components/TicketsList/TicketsList.jsx
+++ src/components/TicketsList/TicketsList.jsx
@@ -7,7 +7,7 @@ let id = 1
 class TicketsList extends Component {
   constructor(props) {
     super(props)
-    this.shownTicketsExist=true
+    this.shownTicketsExist = true
     this.generateTickets = (displayedTickets, tickets, checkedCheckboxesNames) => {
       let filteredTickets = []
       let localTickets = tickets.map((value) => {
@@ -38,7 +38,7 @@ class TicketsList extends Component {
         }
       let components = []
       for (let i = 0; i < displayedTickets && filteredTickets.length; i++) components.push(<Ticket key={id++} ticket={filteredTickets[i]} />)
-      this.shownTicketsExist = components.length?true:false
+      this.shownTicketsExist = components.length ? true : false
       return components
     }
   }
@@ -62,7 +62,13 @@ class TicketsList extends Component {
           />
         )}
         {this.generateTickets(displayedTickets, tickets, checkedCheckboxesNames)}
-        {!this.shownTicketsExist&&<Alert style={{ maxWidth: '504px', marginBottom:'10px', backgroundColor:'#f5f5f5', textAlign:'center', borderColor:'#1890ff45'}} message="Рейсов, подходящих под заданные фильтры, не найдено" type="success" />}
+        {!this.shownTicketsExist && (
+          <Alert
+            style={{ maxWidth: '504px', marginBottom: '10px', backgroundColor: '#f5f5f5', textAlign: 'center', borderColor: '#1890ff45' }}
+            message="Рейсов, подходящих под заданные фильтры, не найдено"
+            type="success"
+          />
+        )}
       </Fragment>
     )
   }
